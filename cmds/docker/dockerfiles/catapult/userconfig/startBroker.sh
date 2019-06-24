889ba239630a2bb13499040b14796a4d35e25743
kaiyzen <daminate@hotmail.com>
2019-06-24 05:28:02 +0000

    updates to have master compose to contain all services

--- cmds/docker/dockerfiles/catapult/userconfig/startBroker.sh
+++ cmds/docker/dockerfiles/catapult/userconfig/startBroker.sh
@@ -2,8 +2,8 @@
 
 ulimit -c unlimited
 
-if [ -e "/state/api-node-broker" ]; then
-  rm -f /state/api-node-broker
+if [ -e "/state/api-node-0-broker" ]; then
+  rm -f /state/api-node-0-broker
 fi
 
 cd /catapult
@@ -12,6 +12,6 @@ ls -alh /data
 cd /data
 rm /data/startup/datadir-initialized
 
-touch /state/api-node-broker
+touch /state/api-node-0-broker
 
 exec /catapult/bin/catapult.broker /userconfig
